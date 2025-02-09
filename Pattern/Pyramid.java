536df195539e30b72001d761fb49617aa90f8e60
bhartitanu <bharatbharti31@gmail.com>
2025-02-10 00:19:54 +0530

    modified:   Pattern/Pyramid.java

--- Pattern/Pyramid.java
+++ Pattern/Pyramid.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Pyramid {
+    // pyramid with number
     public static void NumPyr(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) {
@@ -9,7 +10,49 @@ public class Pyramid {
             for (int j = 1; j <= i; j++) {
                 System.out.print(i + " ");
             }
+            System.out.println();
+        }
+    }
 
+    // Inverted Pyramid
+    public static void Inv_pyr(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = n; j >= i; j--) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void palindrome(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Pascal's Triangle
+    public static void Pascal_tri(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print("  ");
+            }
+            int number = 1;
+            for (int j = 1; j <= i; j++) {
+                System.out.print(number + "  ");
+                number = number * (i - j) / j;
+            }
             System.out.println();
         }
     }
@@ -20,7 +63,16 @@ public class Pyramid {
         int n = sc.nextInt();
 
         // Number Pyramid
-        NumPyr(n);
+        // NumPyr(n);
+
+        // Inverted Number Pyramid
+        // Inv_pyr(n);
+
+        // Palindrome Pattern
+        // palindrome(n);
+
+        // Pascal's Triangle
+        Pascal_tri(n);
     }
 
 }
