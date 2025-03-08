b54394e83ab5daad415f2319919cd1c1f7dbf0e6
bhartitanu <bharatbharti31@gmail.com>
2025-03-08 23:48:29 +0530

    modified:   Basics/Strings.java

--- Basics/Strings.java
+++ Basics/Strings.java
@@ -1,3 +1,15 @@
-public class Strings {
+import java.util.Scanner;
 
+public class Strings {
+    public static void main(String[] args) {
+        String str = "tanu";
+        System.out.println(str);
+        Scanner sc = new Scanner(System.in);
+        String firstname = sc.next();
+        String lastname = sc.next();
+        sc.close();
+        String fullname = firstname + " " + lastname;
+        System.out.println(fullname);
+
+    }
 }
